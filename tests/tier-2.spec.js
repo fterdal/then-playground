ad9ddb953edac1acd4432c66d7e67929246f79a4
Finn <finnterdal@gmail.com>
2019-07-26 10:26:37 -0500

    enforces sequential calls to crayonDraw

--- tests/tier-2.spec.js
+++ tests/tier-2.spec.js
@@ -18,12 +18,24 @@ describe('Tier 2: Sequential', () => {
 
   it('calls crayonDraw four times', done => {
     expect(crayonDraw).to.not.be.called
-    // console.log('crayonDraws', crayonDraws)
     YOUR_CODE_HERE()
     setTimeout(() => {
-      // expect(crayonDraw).callCount(4)
-      // console.log('crayonDraws', crayonDraws)
+      expect(crayonDraw).callCount(4)
       done()
-    }, 900)
+    }, 850)
+  })
+
+  it('successive calls to crayonDraw only after the previous one resolves', done => {
+    expect(crayonDraw).to.not.be.called
+    YOUR_CODE_HERE()
+    setTimeout(() => {
+      expect(crayonDraw).callCount(4)
+      crayonDraws.forEach(({ start }, idx) => {
+        if (!idx) return
+        const prevStart = crayonDraws[idx - 1].start
+        expect(start - prevStart).to.be.greaterThan(199)
+      })
+      done()
+    }, 850)
   })
 })
