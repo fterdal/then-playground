a61dd6e0b7f8f3f14aa94d32aa1c5e0be5f5eb2c
Finn <finnterdal@gmail.com>
2019-08-06 14:55:06 -0500

    await solution for tier 4

--- src/tier-4.js
+++ src/tier-4.js
@@ -21,20 +21,32 @@ const YOUR_CODE_HERE = async colors => {
   const allColors = ['blue', 'green', 'magenta', 'yellow', 'cyan']
 
   // AWAIT
+  try {
+    const firstColors = await Promise.all(
+      colors.map(color => crayonDraw(color))
+    )
+    await Promise.all(
+      allColors
+        .filter(color => !firstColors.includes(color))
+        .map(color => crayonDraw(color))
+    )
+  } catch (err) {
+    handleError(err)
+  }
 
   // THEN
-  Promise.all(colors.map(color => crayonDraw(color)))
-    .then(results => {
-      // console.log(results)
-      return Promise.all(
-        allColors
-          .filter(color => !colors.includes(color))
-          .map(color => crayonDraw(color))
-      )
-    })
-    .catch(err => {
-      handleError(err)
-    })
+  // Promise.all(colors.map(color => crayonDraw(color)))
+  //   .then(results => {
+  //     // console.log(results)
+  //     return Promise.all(
+  //       allColors
+  // .filter(color => !colors.includes(color))
+  // .map(color => crayonDraw(color))
+  //     )
+  //   })
+  //   .catch(err => {
+  //     handleError(err)
+  //   })
 }
 
 module.exports = {
